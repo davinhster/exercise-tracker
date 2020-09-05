983783a90207bc87427f94e891d7edbf203a28f5
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2020-09-05 14:34:57 -0700

    Created Backend for Users and Exercises

---
+++ backend/routes/users.js
@@ -0,0 +1,23 @@
+
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+//finds all the users and res.json return something in json format
+//if error, return error with message
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+
+  const newUser = new User({username});
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
