126a45868c1c914743dc58bf90dabb60b6017dfd
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2020-09-05 20:31:49 -0700

    Added bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
