b7ab260c6f63711df05864e39731c183184be93e
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2020-09-07 20:45:55 -0700

    Changed local to server on heroku

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -26,7 +26,7 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount()  {
-    axios.get('http://localhost:5000/users/')
+    axios.get('https://vinhserver.herokuapp.com/users/')
     .then(response => {
       if (response.data.length > 0) {
         this.setState({ 
@@ -72,7 +72,7 @@ export default class CreateExercise extends Component {
     }
     
     console.log(exercise);
-    axios.post('http://localhost:5000/exercises/add', exercise)
+    axios.post('https://vinhserver.herokuapp.com/exercises/add', exercise)
       .then(res => console.log(res.data));
     window.location = '/';
 
